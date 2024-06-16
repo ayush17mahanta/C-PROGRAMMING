97cad56b69d0e854bd3a8e8add46b22c35cf1266
Ayush Mahanta <ayush17mahanta@gmail.com>
2024-06-16 20:50:05 +0530

    Update calculator.cpp

--- CodSoft/calculator.cpp
+++ CodSoft/calculator.cpp
@@ -1,63 +1,51 @@
 #include <iostream>
 using namespace std;
 
-char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-
-void displayBoard() {
-    for (int i = 0; i < 9; i += 3) {
-        cout << " " << board[i] << " | " << board[i + 1] << " | " << board[i + 2] << endl;
-        if (i < 6) cout << "---|---|---" << endl;
-    }
-}
-
-bool checkWin(char player) {
-    for (int i = 0; i < 3; ++i)
-        if ((board[i] == player && board[i + 3] == player && board[i + 6] == player) ||
-            (board[3 * i] == player && board[3 * i + 1] == player && board[3 * i + 2] == player))
-            return true;
-    return (board[0] == player && board[4] == player && board[8] == player) ||
-           (board[2] == player && board[4] == player && board[6] == player);
-}
-
-bool checkDraw() {
-    for (char cell : board) if (cell == ' ') return false;
-    return true;
-}
-
-void playGame() {
-    char currentPlayer = 'X';
-    while (true) {
-        displayBoard();
-        int move;
-        cout << "Player " << currentPlayer << ", enter your move (1-9): ";
-        cin >> move;
-        move--;
-        if (move < 0 || move >= 9 || board[move] != ' ') {
-            cout << "Invalid move. Try again." << endl;
-            continue;
-        }
-        board[move] = currentPlayer;
-        if (checkWin(currentPlayer)) {
-            displayBoard();
-            cout << "Player " << currentPlayer << " wins!" << endl;
-            break;
-        }
-        if (checkDraw()) {
-            displayBoard();
-            cout << "It's a draw!" << endl;
-            break;
-        }
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    }
-}
-
 int main() {
-    char playAgain;
-    do {
-        fill(begin(board), end(board), ' ');  // Reset the board
-        playGame();
-        cout << "Do you want to play again? (y/n): ";
-        cin >> playAgain;
-    } while (playAgain == 'y' || playAgain == 'Y');
+    char operation;
+    double num1, num2, result;
+
+    cout << "Enter first number: ";
+    while (!(cin >> num1)) {
+        cout << "Invalid input. Please enter a number: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+
+    cout << "Enter second number: ";
+    while (!(cin >> num2)) {
+        cout << "Invalid input. Please enter a number: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+                return 1;
+            }
+            break;
+        default:
+            cout << "Invalid operation. Please choose from +, -, *, /." << endl;
+            return 1;
+    }
+
+    cout << "Result: " << result << endl;
+
     return 0;
 }
